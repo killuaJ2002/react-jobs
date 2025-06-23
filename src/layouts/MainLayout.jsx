75967046ef44b3702675e78623581e4baa1fde19
killuaJ2002 <kjcodes2002@gmail.com>
2025-06-23 12:47:02 +0530

    implemented edit job page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 const MainLayout = () => {
@@ -6,6 +8,7 @@ const MainLayout = () => {
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 };
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,7 +1,7 @@
-import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom";
+import { useLoaderData, Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 const JobPage = ({ deleteJob }) => {
-  //   const { id } = useParams();
   const job = useLoaderData();
   const navigate = useNavigate();
   const onDeleteClick = (jobId) => {
@@ -10,6 +10,7 @@ const JobPage = ({ deleteJob }) => {
     );
     if (!confirm) return;
     deleteJob(jobId);
+    toast.success("Job deleted successfully");
     return navigate("/jobs");
   };
   return (
@@ -82,7 +83,7 @@ const JobPage = ({ deleteJob }) => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
